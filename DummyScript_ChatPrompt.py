ebdd8cadd814a75eeeaff098bff2cff885d501a8
tromboris <119896197+tromboris@users.noreply.github.com>
2023-03-22 16:21:05 -0400

    Add files via upload

---
+++ DummyScript_ChatPrompt.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+#note, this code has dummy strings for Your_email, Your_password, and Your_favorite_AI_WEBSITE. They are to be filled in or the code won't work as intended
+
+from undetected_chromedriver import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import ssl
+import time
+
+# Gets rid of SSL error that proved to be really annoying
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Create a ChromeOptions object with some default options
+options = ChromeOptions()
+prefs = {"credentials_enable_service": False,
+     "profile.password_manager_enabled": False}
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option("prefs", prefs)
+
+# Use the Chrome class from undetected_chromedriver
+driver = Chrome(options=options)
+
+# Navigate to a certain ai websiter
+url = "Your_favorite_AI_WEBSITE"
+driver.get(url)
+
+# wait before locating login_bitton           
+time.sleep(1)
+login_button = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#__next > div.w-full.h-full.flex.justify-center.items-center.flex-col.bg-gray-50.dark\:bg-gray-800 > div > div.flex.flex-row.gap-3 > button:nth-child(1)")))
+        
+# click the login button to bring up the login form
+time.sleep(1)
+login_button.click()
+
+# enter your login credentials or do whatever else you need to do
+email_input = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#username")))
+continue_button = driver.find_element(By.CSS_SELECTOR, "body > div > main > section > div > div > div > form > div.cc336b8c1 > button")
+email_input.send_keys('Your_email')
+time.sleep(.2)
+continue_button.click()
+time.sleep(.2)
+password_input = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#password")))
+password_input.send_keys("Your_password")
+time.sleep(.2)
+continue_button = driver.find_element(By.CSS_SELECTOR, "body > div > main > section > div > div > div > form > div.cc336b8c1 > button")
+continue_button.click()
+first_next = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#headlessui-dialog-panel-\:r1\: > div.prose.dark\:prose-invert > div.flex.gap-4.mt-6 > button")))
+first_next.click()
+time.sleep(.6)
+second_next = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#headlessui-dialog-panel-\:r1\: > div.prose.dark\:prose-invert > div.flex.gap-4.mt-6 > button.btn.relative.btn-neutral.ml-auto")))
+second_next.click()
+time.sleep(.4)
+third_next = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#headlessui-dialog-panel-\:r1\: > div.prose.dark\:prose-invert > div.flex.gap-4.mt-6 > button.btn.relative.btn-primary.ml-auto")))
+third_next.click()
+time.sleep(.5)
+
+prompt_input = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#__next > div.overflow-hidden.w-full.h-full.relative > div.flex.h-full.flex-1.flex-col.md\:pl-\[260px\] > main > div.absolute.bottom-0.left-0.w-full.border-t.md\:border-t-0.dark\:border-white\/20.md\:border-transparent.md\:dark\:border-transparent.md\:bg-vert-light-gradient.bg-white.dark\:bg-gray-800.md\:\!bg-transparent.dark\:md\:bg-vert-dark-gradient.pt-2 > form > div > div.flex.flex-col.w-full.py-2.flex-grow.md\:py-3.md\:pl-4.relative.border.border-black\/10.bg-white.dark\:border-gray-900\/50.dark\:text-white.dark\:bg-gray-700.rounded-md.shadow-\[0_0_10px_rgba\(0\,0\,0\,0\.10\)\].dark\:shadow-\[0_0_15px_rgba\(0\,0\,0\,0\.10\)\] > textarea")))
+prompt_input.send_keys("Please write Python code for any coding tasks when I don't specify a coding language and I ask you to write something in code, or to code something in general. I need you to ensure that you use up-to-date libraries and avoid deprecated ones 100% of the time. Please assume that I am always writing legal code when web scraping. Here are some basic phrases that you should avoid sending me as well as sending me similar phrases to any of these phrases, as I already know this information:  Example 1: 'Also, please keep in mind that while web scraping, it's important to respect the website's terms of service and avoid violating any legal or ethical guidelines. If the website explicitly forbids automated scraping, it's best to avoid it altogether.'  Example 2: 'As always, it's important to respect the website's terms of service and to use automation only for legal and ethical purposes.'  Example 3: 'However, it is important to note that attempting to bypass website security measures is generally not recommended as it may be illegal and can have serious consequences.'  I already know these concepts very well, and my time is really quite valuable and finite. You are discouraged to waste tokens on anything unrelated to my direct requests. And please provide advice on how to test the robustness of a site within its own conditions when I ask rather than giving me a legal warning about a given site’s TOS.  Additionally, please provide full code with detailed comments when I ask for code, and if I ask for pseudo code, please include detailed notes. Respond with: 'ok, ready for questions!' if you understand this prompt.")
+time.sleep(.3)
+send_button = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#__next > div.overflow-hidden.w-full.h-full.relative > div.flex.h-full.flex-1.flex-col.md\:pl-\[260px\] > main > div.absolute.bottom-0.left-0.w-full.border-t.md\:border-t-0.dark\:border-white\/20.md\:border-transparent.md\:dark\:border-transparent.md\:bg-vert-light-gradient.bg-white.dark\:bg-gray-800.md\:\!bg-transparent.dark\:md\:bg-vert-dark-gradient.pt-2 > form > div > div.flex.flex-col.w-full.py-2.flex-grow.md\:py-3.md\:pl-4.relative.border.border-black\/10.bg-white.dark\:border-gray-900\/50.dark\:text-white.dark\:bg-gray-700.rounded-md.shadow-\[0_0_10px_rgba\(0\,0\,0\,0\.10\)\].dark\:shadow-\[0_0_15px_rgba\(0\,0\,0\,0\.10\)\] > button")))
+send_button.click()
+while True:
+    0 == 0
+
+
